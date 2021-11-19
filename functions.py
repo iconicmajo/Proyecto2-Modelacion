2400b7f504d60616c9a3596cf3a374d6c933e152
iconicmajo <cas181202@uvg.edu.gt>
2021-11-19 00:06:09 -0600

    Adding data and jupyter

---
+++ functions.py
@@ -0,0 +1,14 @@
+#Maria Jose Castro 181202
+#Juan Fernando De Leon 17250
+#Christopher Barrios #18
+
+#Function File
+
+def polinomial():
+  print('modelo polinimial')
+
+def logaritmico():
+  print('modelo logaritmico')
+
+def exponencial():
+  print('modelo exponencial')
---
+++ main.py
@@ -0,0 +1,12 @@
+#Maria Jose Castro 181202
+#Juan Fernando De Leon 17250
+#Christopher Barrios #18
+
+from functions import *
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("moneda.csv")
+
+accracy_lev = input("Ingrese Nivel de Confianza")
+
